a1dfd557b73e2de4cc06b17eba079dbaa05cf58c
jinobaby <jbaby2527@conestogac.on.ca>
2023-11-17 17:16:51 -0500

    Anglecheck

---
+++ TriangleAngleChecker/TriangleAngleChecker.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleAngleChecker
+{
+	TEST_CLASS(TriangleAngleChecker)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
